60b49bc61aa173914cc94fd2b511dd99f9d96a83
polar-bear-ytk <yukidaruma.sky@gmail.com>
2020-12-24 10:43:39 +0900

    簡易ログイン

---
+++ easylogin2/src/main/java/com/example/easylogin/Easylogin2Application.java
@@ -0,0 +1,13 @@
+package com.example.easylogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Easylogin2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Easylogin2Application.class, args);
+	}
+
+}
